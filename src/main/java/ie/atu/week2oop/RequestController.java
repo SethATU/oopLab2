d3be493d4d5fb416050ea36b42730a0432cc9dd1
SethButler-STUDENT <g00467621@atu.ie>
2026-02-09 11:34:14 +0000

    added rest controller

--- src/main/java/ie/atu/week2oop/RequestController.java
+++ src/main/java/ie/atu/week2oop/RequestController.java
@@ -1,4 +1,16 @@
 package ie.atu.week2oop;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class RequestController {
+
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "Hello World";
+    }
 }
