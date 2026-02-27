ed2be42b6f3f1eda0ed59ea9e492285baf298245
SuperButler <g00467621@atu.ie>
2026-02-27 23:43:56 +0000

    Using @PathVariable

--- src/main/java/ie/atu/week2oop/RequestController.java
+++ src/main/java/ie/atu/week2oop/RequestController.java
@@ -1,6 +1,7 @@
 package ie.atu.week2oop;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class RequestController {
 
+    /// Basic rest controller
     @GetMapping("/hello")
     public String hello()
     {
         return "Hello World";
+    }
+
+    /// Using a path variable
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name) {
+        return "Hello " + name + "!";
     }
 }
